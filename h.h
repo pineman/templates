1bddc12b7072584499419310e7a38b5e9dde9ea1
João Pinheiro <joao.castropinheiro@gmail.com>
2017-04-01 19:26:07 +0100

    Initial commit

---
+++ h.h
@@ -0,0 +1,4 @@
+#ifndef _<FILE>_H
+#define _<FILE>_H
+
+#endif // _<FILE>_H
---
+++ python.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# vim: ts=4 sts=4 sw=4 et
+
+"""
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
